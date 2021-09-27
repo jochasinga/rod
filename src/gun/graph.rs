94a920341fba87aaa20198a37a504ccccc7d52ec
Joe Chasinga <jo.chasinga@gmail.com>
2021-09-26 18:40:57 -0700

    Pass first graph test

--- src/gun/graph.rs
+++ src/gun/graph.rs
@@ -7,7 +7,7 @@ use std::collections::HashSet;
 use std::hash::Hash;
 use generational_arena::Arena;
 
-pub type VertexSet<T> = HashSet<T>;
+pub type VertexSet<T> = Set<Vertex<T>>;
 pub type EdgeSet<T> = Arena<Edge<T>>;
 
 struct Graph<T> {
@@ -26,12 +26,12 @@ where
 {
     pub fn new() -> Self {
         GraphBuilder {
-            vertices: HashSet::new(),
+            vertices: Set::new(),
             edges: Arena::new(),
         }
     }
 
-    fn add_vertex(mut self, v: T) -> Self {
+    fn add_vertex(mut self, v: Vertex<T>) -> Self {
         self.vertices.insert(v);
         self
     }
@@ -58,7 +58,7 @@ where
 {
     fn new() -> Self {
         Graph {
-            vertices: HashSet::new(),
+            vertices: Set::new(),
             edges: Arena::new(),
         }
     }
@@ -66,23 +66,88 @@ where
 
 // A vertex A is a finite non-empty set
 // {A, B, C, D}
-// #[derive(PartialEq, Eq, Hash)]
-pub type Vertex<T> = HashSet<T>;
+#[derive(PartialEq, Eq, Hash, Clone)]
+pub struct Vertex<T>(pub T);
+
+impl<T> Vertex<T> 
+where 
+    T: Eq + Hash + Clone,
+{
+    fn new(data: T) -> Self {
+        Vertex(data)
+    }
+}
+
+#[derive(Clone)]
+pub struct Set<T>(pub HashSet<T>);
+
+impl<T> Set<T>
+where 
+    T: Eq + Hash + Clone,
+{
+    fn new() -> Self {
+        Set(HashSet::new())
+    }
+
+    fn insert(&mut self, data: T) {
+        let Set(inner) = self;
+        inner.insert(data);
+        let new_inner = inner.clone();
+        *self = Set(new_inner);
+    }
+
+    fn len(&self) -> usize {
+        let Set(hs) = self;
+        hs.len()
+    }
+
+    fn is_subset(&self, other: &Set<T>) -> bool {
+        let (Set(h1), Set(h2)) = (self, other);
+        h1.is_subset(h2)
+    }
+}
+
+impl<T> From<&Edge<T>> for Set<T>
+where
+    T: Eq + Hash + Clone,
+{
+    fn from(e: &Edge<T>) -> Self {
+        let Edge(v1, v2) = e;
+        let mut hs = HashSet::<Vertex<T>>::new();
+        hs.insert(v1.clone());
+        hs.insert(v2.clone());
+        let vs: VertexSet<T> = Set(hs);
+        vs.into()
+    }
+}
+
+impl<T> From<VertexSet<T>> for Set<T>
+where 
+    T: Eq + Hash + Clone,
+{
+    fn from(vst: VertexSet<T>) -> Self {
+        let Set(vs) = vst;
+        let mut hs = HashSet::<T>::new();
+        for v in vs.iter() {
+            let Vertex(t) = v;
+            let t_ = t.clone();
+            hs.insert(t_);
+        }
+        Set(hs)
+    }
+}
 
 /// An edge connects {A, B} where A and B are elements of a set of vertices V.
 /// {A, B} ⊆ V where A ∈ V & B ∈ V
 #[derive(PartialEq, Eq, Hash, Clone)]
-pub struct Edge<T>(pub T, pub T);
+pub struct Edge<T>(pub Vertex<T>, pub Vertex<T>);
 
-impl<T> Into<HashSet<T>> for &Edge<T> 
+impl<T> Edge<T> 
 where 
     T: Eq + Hash + Clone,
 {
-    fn into(self) -> HashSet<T> {
-        let mut set = HashSet::new();
-        set.insert(self.0.clone());
-        set.insert(self.1.clone());
-        set
+    fn new(v1: Vertex<T>, v2: Vertex<T>) -> Self {
+        Edge(v1, v2)
     }
 }
 
@@ -93,13 +158,49 @@ fn check_graph<T: Eq + Hash + Clone>(vertices: VertexSet<T>, edges: EdgeSet<T>) 
     }
 
     for (_, e) in edges.iter() {
-        let h: HashSet<T> = (e).into();
-        if h.len() > 2 {
+        let vertices_ = vertices.clone();
+        let s: Set<T> = e.into();
+        if s.len() > 2 {
             return false;
         }
-        if !h.is_subset(&vertices) {
+        if !s.is_subset(&vertices_.into()) {
             return false;
         }
     }
     true
 }
+
+#[cfg(test)]
+pub mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_graph() -> Result<(), String> {
+        let v1 = Vertex::new(33);
+        let v2 = Vertex::new(22);
+        let v3 = Vertex::new(11);
+        let v1_ = v1.clone();
+        let v1__ = v1.clone();
+        let v2_ = v2.clone();        
+        let v2__ = v2.clone();
+        let v3_ = v3.clone();
+        let v3__ = v3.clone();
+        
+        let e1 = Edge::new(v1, v2);
+        let e2 = Edge::new(v2_, v3);
+        let e3 = Edge::new(v3_, v1_);
+
+        let graph = GraphBuilder::<i32>::new()
+            .add_vertex(v1__)
+            .add_vertex(v2__)
+            .add_vertex(v3__)
+            .add_edge(e1)
+            .add_edge(e2)
+            .add_edge(e3)
+            .build()?;
+
+        Ok(())
+    }
+}
+
