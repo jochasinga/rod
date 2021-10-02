4360757b3e20aebdb1df439ee4d03de961cf4f54
Márk Kővári <kovarimarkofficial@gmail.com>
2021-10-02 13:32:13 +0200

    Test/set intersection functions (#5)
    * test: set insert fucntions
    
    * test: intersection functions

--- src/gun/graph/set/mod.rs
+++ src/gun/graph/set/mod.rs
@@ -140,4 +140,90 @@ mod tests {
         numbers.insert(2u8);
         assert_eq!(numbers.len(), 2);
     }
+
+    #[test]
+    fn is_subset_with_empty_sets() {
+        let empty_first = Set::<u8>::new();
+        let empty_second = Set::<u8>::new();
+        assert_eq!(empty_first.is_subset(&empty_second), true);
+    }
+
+    #[test]
+    fn is_subset_should_be_false_with_non_empty_is_subset_of_empty() {
+        let mut non_empty = Set::<u8>::new();
+        non_empty.insert(1u8);
+        let empty = Set::<u8>::new();
+        assert_eq!(non_empty.is_subset(&empty), false);
+    }
+
+    #[test]
+    fn is_subset_should_be_true_with_empty_is_subset_of_non_empty() {
+        let mut non_empty = Set::<u8>::new();
+        non_empty.insert(1u8);
+        let empty = Set::<u8>::new();
+        assert_eq!(empty.is_subset(&non_empty), true);
+    }
+
+    #[test]
+    fn intersection_of_two_empty_set_should_be_empty() {
+        let empty_first = Set::<u8>::new();
+        let empty_second = Set::<u8>::new();
+
+        assert_eq!(empty_first.intersection(&empty_second).len(), 0);
+    }
+
+    #[test]
+    fn intersection_of_one_non_empty_and_empty_set_should_be_the_non_empty_set() {
+        let mut non_empty = Set::<u8>::new();
+        non_empty.insert(2u8);
+        non_empty.insert(1u8);
+        let empty = Set::<u8>::new();
+
+        assert_eq!(non_empty.intersection(&empty), non_empty);
+    }
+
+    #[test]
+    fn intersection_of_two_none_empty_with_no_common_elements_should_be_the_intersection_of_the_two_set_count(
+    ) {
+        let mut non_empty_first = Set::<u8>::new();
+        non_empty_first.insert(1u8);
+        non_empty_first.insert(2u8);
+
+        let mut non_empty_second = Set::<u8>::new();
+        non_empty_second.insert(3u8);
+        non_empty_second.insert(4u8);
+
+        let mut expected_intersection = Set::<u8>::new();
+        expected_intersection.insert(1u8);
+        expected_intersection.insert(2u8);
+        expected_intersection.insert(3u8);
+        expected_intersection.insert(4u8);
+
+        assert_eq!(
+            non_empty_first.intersection(&non_empty_second),
+            expected_intersection
+        );
+    }
+
+    #[test]
+    fn intersection_of_two_none_empty_with_common_elements_count_should_be_the_intersection_of_the_two_set_count(
+    ) {
+        let mut non_empty_first = Set::<u8>::new();
+        non_empty_first.insert(1u8);
+        non_empty_first.insert(2u8);
+
+        let mut non_empty_second = Set::<u8>::new();
+        non_empty_second.insert(1u8);
+        non_empty_second.insert(4u8);
+
+        let mut expected_intersection = Set::<u8>::new();
+        expected_intersection.insert(1u8);
+        expected_intersection.insert(2u8);
+        expected_intersection.insert(4u8);
+
+        assert_eq!(
+            non_empty_first.intersection(&non_empty_second),
+            expected_intersection
+        );
+    }
 }
